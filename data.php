16b5ae4582b7ffdf929d1ac33706c86dc50882dc
ABSylvain <arnaudbouriquesylvain@gmail.com>
2017-06-04 23:44:21 +0200

    formulaire fonctionne : - Creer un doc dans dossier; !- faire en sorte d'organiser les données user !- modif manquant !- supp manquant

--- data.php
+++ data.php
@@ -1,16 +1,17 @@
 <?php 
     //***** Pour le password
-    if(isset($_POST['mot_de_passe']) && $_POST['mot_de_passe'] ==  "password") {
-        //***** Rappel des elements du formulaire
-        echo $_POST['speudo'];
-        echo $_POST['message'];
-        echo $_POST['game'];
-        echo $_POST['case'];
-        echo $_POST['yesno'];
+    if(isset($_POST['mot_de_passe']) && $_POST['mot_de_passe'] ==  "bob") {
+        //***** 
+        $monfichier = fopen('compte/' . $_POST['speudo']. '.txt', 'w');
+        //***** Colle les elements du formulaire
+        fputs($monfichier, $_POST['speudo']);
+        fputs($monfichier, $_POST['message']);
+        fputs($monfichier, $_POST['game']);
+        fputs($monfichier, $_POST['case']);
+        fputs($monfichier, $_POST['yesno']);
+        fclose($monfichier);
+        //***** Sinon message 'lost'
     }else{
         echo 'LOST';
-    }
-    
-
-    
+    };
 ?>
