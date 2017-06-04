437a6c757d73c03d56670e7b208c2059a68c395b
ABSylvain <arnaudbouriquesylvain@gmail.com>
2017-06-04 23:04:23 +0200

    formulaire fini debut de recolte info

--- data.php
+++ data.php
@@ -1,9 +1,16 @@
 <?php 
-    echo $_POST['speudo'];
-    echo $_POST['message'];
-    echo $_POST['game'];
-    echo $_POST['case'];
-    echo $_POST['yesno'];
+    //***** Pour le password
+    if(isset($_POST['mot_de_passe']) && $_POST['mot_de_passe'] ==  "password") {
+        //***** Rappel des elements du formulaire
+        echo $_POST['speudo'];
+        echo $_POST['message'];
+        echo $_POST['game'];
+        echo $_POST['case'];
+        echo $_POST['yesno'];
+    }else{
+        echo 'LOST';
+    }
+    
 
     
 ?>
