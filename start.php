62c60f18e23c94587c3d4beaff8d5aa270fb1b51
ABSylvain <arnaudbouriquesylvain@gmail.com>
2017-06-04 21:46:01 +0200

    formulairePHP

---
+++ start.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Start</title>
+</head>
+<body>
+    <form>
+        <input type="text" action="data.php" name ="speudo">
+        <input type="submit" name="btn" value="Submit"></input>
+    </form>
+    <?php 
+    
+    ?>
+</body>
+</html>
