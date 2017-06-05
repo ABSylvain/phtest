f305ee32372330865ba679926b8b3281baeff442
ABSylvain <arnaudbouriquesylvain@gmail.com>
2017-06-05 03:07:15 +0200

    ajout commentaire

--- index.php
+++ index.php
@@ -8,15 +8,21 @@
 </head>
 <body>
     <?php 
+        //***** On ouvre le dossier dans une variable pour le rappeler
         if($folder = opendir('./compte')){
+            //***** On parcours les fichier du dossier, chaque iteration iras dans $fichier
             while(false !== ($fichier = readdir($folder))){
+                //***** A chaque fichier on verifi que sa ne soi pas un fichier indesirable 
                 if($fichier != '.' && $fichier != '..'&& $fichier != '.DS_Store' && $fichier  != 'index.php'){
+                        //***** On place le nom du ficier dans un lien qui menne au fichier en question
                         echo '<p><a href="./compte/' . $fichier . '">' . $fichier . '</a></p>';  
                                                                                     } 
-                                                            } 
-                                            closedir($folder);
-                                            }else
-                                                echo 'Le dossier n\' a pas pu être ouvert';
+                                                            }
+                            //***** On ferme le dossier
+                            closedir($folder);
+            //***** si opendir n'as pas marcher
+            }else
+                echo 'Le dossier n\' a pas pu être ouvert';
      ?>
 </body>
 </html>
