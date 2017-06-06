e05b43e8a4351a1008c589749c246e890c876d5d
ABSylvain <arnaudbouriquesylvain@gmail.com>
2017-06-06 14:48:22 +0200

    modif/supp op

--- form/data.php
+++ form/data.php
@@ -11,7 +11,6 @@
     <div id="op">
         <h3>Votre compte a bien été creer/modifier.</h3>
         <h5>Cliquer sur ce bouton pour retourner a la liste.</h5>
-        <?php echo $value ?>
         <a href="index.php">Retour</a>
     </div>
      
@@ -19,22 +18,25 @@
     //***** Pour le password
     if(isset($_POST['mot_de_passe']) && $_POST['mot_de_passe'] ==  "bob") {
         //***** On chope les elments du formulaire
+        $oldname = $_POST['oldname'];
         $speudo = $_POST['speudo'];
         $message = $_POST['message'];
         $game = $_POST['game'];
         $case = [];
-        foreach ($_POST['case'] as $value)
-            {
-              $value = $case;
-            }
         $yesno = $_POST['yesno'];
         $age = $_POST['age'];
+
+        //***** Supp le fichier selectionner
+        if(is_file('compte/'. $oldname. '.php')){
+            unlink('compte/' . $oldname. '.php');
+        }
         //***** Creer un fichier 'txt' avec speudo comme nom et dans une variable
         $monfichier = fopen('compte/' . $_POST['speudo']. '.php', 'w');
         //***** Colle les elements du formulaire (copi/colle le formulaire d'origine 
         //***** avec les variable de recu du POST)
         fputs($monfichier, '<link rel="stylesheet" type="text/css" href="../style.css" /> 
                             <form action="../data.php" method="POST">
+                            <input type="hidden" name="oldname" value="'.$speudo.'">
                                     <input type="text" name ="speudo" placeholder="'.$speudo.'" class="bar">
                                     <input type="password" name="mot_de_passe" placeholder="Password" class="bar"/>
                                     <input type="number" name="age" placeholder="'.$age.'" class="bar"/>
@@ -48,7 +50,6 @@
                                         <option value="Diablo3">Diablo*RiP*</option>
                                     </select>
                                     
-                                    <p id="choix">L\' ancien choix : '.$case.'</p>
                                     <input type="checkbox" name="case" class="point"/><label for="case">Clavier/Souris</label>
                                     <input type="checkbox" name="case" class="point"/><label for="case">Manette</label>
                                     <input type="checkbox" name="case" class="point"/><label for="case">Gestuel</label>
@@ -76,11 +77,6 @@
     
     };
 ?>
-<div id="op">
-        <h3>Votre compte a bien été creer/modifier.</h3>
-        <h5>Cliquer sur ce bouton pour retourner a la liste.</h5>
-        <?php echo $case ?>
-        <a href="index.php">Retour</a>
-    </div>
+
 </body>
 </html>
